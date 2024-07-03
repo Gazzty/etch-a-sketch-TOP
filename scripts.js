b5c62b6b70a8b5d7cf83e02021c38052183263f5
Gustavo Muiños <tomasgustavo99@gmail.com>
2024-07-03 18:17:47 -0300

    Finished project, added border settings so it doesn't get croudy. Also added multiple check methods to make it more stable.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,61 @@
 const mainContainer = document.querySelector(".main-container");
 
 // Create all divs for the grid
-let row = 16;
-let columns = 16;
-for (let i = 0; i < (row * columns); i++) {
-    const block = document.createElement("div");
-    block.classList.add("block");
-    mainContainer.appendChild(block);
+let rows = 16;
+let addBorder = true;
+
+// Function declaration
+// Hover effect on blocks
+function blockColorSetting() {
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+        block.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = 'blue';
+        })
+    })
+}
+function createSketchPad(rows) {
+
+    // First, delete previous sketchpad
+    const previousBlocks = document.querySelectorAll(".block");
+    previousBlocks.forEach((block) => {
+        block.remove();
+    })
+
+    for (let i = 0; i < (Math.pow(rows, 2)); i++) {
+        const block = document.createElement("div");
+        block.classList.add("block");
+        // Set block size
+        block.style.flex = `1 0 ${100 / rows}%`;
+        block.style.paddingTop = `${100 / rows}%`;
+        // Set border
+        if(addBorder){
+            block.style.border = "1px solid";
+        }
+        mainContainer.appendChild(block);
+    }
+
+    blockColorSetting();
+
+    return;
 }
 
-// Hover effect on blocks
-const blocks = document.querySelectorAll(".block");
-blocks.forEach((block) => {
-    block.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = 'blue';
-    })
+// Start with base sketchpad
+createSketchPad(rows);
+
+// Ask user for new sketch dimension
+const createButton = document.querySelector("#create-sketchpad-button");
+createButton.addEventListener(("click"), () => {
+    rows = prompt("Please write the number of columns per row you want");
+    // Check that rows are under 100
+    while(rows > 100 || rows <= 0 || !Number.isInteger(+rows)){
+        alert("Keep canvas size under 100 and above 0");
+        rows = prompt("Please write the number of columns per row you want");
+    }
+    if(rows > 30){
+        addBorder = false;
+    }else{
+        addBorder = true;
+    }
+    createSketchPad(rows, addBorder);
 })
